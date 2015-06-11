d030348bfac0866fa8d54422b236cdeb9eb0e40b
wdysln <zirkovandersen@gmail.com>
2015-06-12 01:50:55 +0300

    vpx

--- media-libs/libvpx/libvpx-1.4.0.py
+++ media-libs/libvpx/libvpx-1.4.0.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ The VP8 Codec SDK
 homepage @ http://www.webmproject.org/
 license @ BSD
-src_url @ http://webm.googlecode.com/files/$name-v$version.tar.bz2
+src_url @ https://github.com/webmproject/$name/archive/v$version.tar.gz
 arch @ ~x86_64
 options @ altivec debug mmx postproc sse sse2 sse3 ssse3 sse4_1 threads cpudetection
 """
