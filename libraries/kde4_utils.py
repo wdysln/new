9d0ad1fb150ea92b71e024834b549af5aafcc917
wdysln <zirkovandersen@gmail.com>
2015-06-13 01:53:53 +0300

    libical

--- libraries/kde4_utils.py
+++ libraries/kde4_utils.py
@@ -45,7 +45,7 @@ def kde4_utils_prepare():
 
 def kde4_utils_configure():
     cd("build")
-    cmake_conf("-DAutomoc4_DIR=/usr/lib/automoc4  \    
+    cmake_conf("-DAutomoc4_DIR=/usr/lib/automoc4    
 		-DCMAKE_SKIP_RPATH=ON \
                 -Wno-dev",sourcedir=build_dir+"/%s" % subdir)
 
