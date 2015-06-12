6943f770a0a294f8e04df59bae5a7437700f668a
wdysln <zirkovandersen@gmail.com>
2015-06-13 02:28:42 +0300

    kde

--- dev-libs/cyrus-sasl/cyrus-sasl-2.1.26.py
+++ dev-libs/cyrus-sasl/cyrus-sasl-2.1.26.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ Cyrus Simple Authentication Service Layer (SASL) library
 homepage @ http://cyrusimap.web.cmu.edu/downloads.html#sasl
 license @ as-is
-src_url @ ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/$fullname.tar.gz
+src_url @ ftp://ftp.cyrusimap.org/cyrus-sasl/$fullname.tar.gz
 arch @ ~x86_64
 """
 
