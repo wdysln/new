272fc85ad0c18ffe1b1db5acef149845e751fb2a
wdysln <zirkovandersen@gmail.com>
2015-06-13 01:56:47 +0300

    libical

--- kde-base/kde4/kde4-4.13.4.py
+++ kde-base/kde4/kde4-4.13.4.py
@@ -7,7 +7,7 @@ arch @ ~x86_64
 
 depends = """
 runtime @ >=kde-base/kde-runtime-4.14.3 >=kde-base/kdelibs-4.14.3 >=kde-base/kde-workspace-4.11.4
-x11-base/xorg-server >=kde-base/ark-4.14.3 >=kde-base/kde-baseapps-4.14.3
+x11-base/xorg-server >=kde-base/kde-baseapps-4.14.3
 """
 
 standard_procedure = False
