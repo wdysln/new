858854a526ce8b3ce13786bc2e293917ccd17a86
wdysln <zirkovandersen@gmail.com>
2015-06-13 01:51:04 +0300

    libical

--- dev-libs/libical/libical-1.0.1.py
+++ dev-libs/libical/libical-1.0.1.py
@@ -5,5 +5,8 @@ license @ LGPL-2 MPL-2
 src_url @ https://github.com/libical/libical/releases/download/v1.0.1/libical-1.0.1.tar.gz
 arch @ ~x86_64
 """
+depends = """
+build @ dev-util/cmake
+"""
 
 get("main/cmake_utils")
