5d45a2b187f27418cd3009866edefd0962e4a823
wdysln <zirkovandersen@gmail.com>
2015-06-13 03:30:43 +0300

    attica

--- kde-base/attica/attica-0.4.2.py
+++ kde-base/attica/attica-0.4.2.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ A library to access Open Collaboration Service providers
 homepage @ http://www.kde.org/
 license @ LGPL
-src_url @ ftp://ftp.kde.org/pub/kde/stable/$name/$fullname.tar.bz2
+src_url @ http://download.kde.org/stable/$name/$fullname.tar.bz2
 arch @ ~x86_64
 """
 
