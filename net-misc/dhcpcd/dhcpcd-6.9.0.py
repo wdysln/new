c0334cd46ce69f2e1f7283f6b23cc7fb5d17f00c
wdysln <zirkovandersen@gmail.com>
2015-06-14 18:26:43 +0300

    dhcpcd

---
+++ net-misc/dhcpcd/dhcpcd-6.9.0.py
@@ -0,0 +1,21 @@
+metadata = """
+summary @ RFC2131 compliant DHCP client daemon
+homepage @ http://roy.marples.name/dhcpcd/
+license @ BSD
+src_url @ http://roy.marples.name/downloads/$name/$fullname.tar.bz2
+arch @ ~x86_64
+"""
+"""
+prepare = lambda: patch(level=1)
+"""
+def configure():
+    raw_configure("--libexecdir=/usr/lib/dhcpcd",
+            "--dbdir=/var/lib/dhcpcd", "--rundir=/run")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    makesym("/sbin/dhcpcd", "/usr/bin/dhcpcd")
+    insfile("%s/dhcpcd.conf.d" % filesdir, "/etc/conf.d/dhcpcd")
+    echo("noipv4ll", "/etc/dhcpcd.conf")
+    insfile("%s/dhcpcd_.service" % filesdir, "/usr/lib/systemd/system/dhcpcd@.service")
+    insfile("%s/dhcpcd.service" % filesdir, "/usr/lib/systemd/system/dhcpcd.service")
