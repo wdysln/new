4573e323aeb0cbae566c8555652a0b00689783c9
wdysln <zirkovandersen@gmail.com>
2015-06-14 23:16:37 +0300

    sayanora

--- media-sound/sayanora/sayanora-0.6.6.py
+++ media-sound/sayanora/sayanora-0.6.6.py
@@ -7,10 +7,7 @@ arch @ ~x86_64
 """
 
 depends = """
-build @ dev-util/desktop-file-utils 
-        x11-themes/hicolor-icon-theme media-libs/pango x11-libs/cairo x11-libs/gdk-pixbuf media-libs/alsa-lib
-        media-libs/imlib2 media-libs/libvorbis media-libs/libogg media-video/ffmpeg
-        media-plugins/gst-plugins-bad:0 media-plugins/gst-plugins-base:0 media-plugins/gst-plugins-good:0 media-plugins/gst-plugins-bad:0
+build @ media-plugins/gst-plugins-bad:0 media-plugins/gst-plugins-base:0 media-plugins/gst-plugins-good:0 media-plugins/gst-plugins-bad:0
         media-plugins/gst-plugins-ugly:0 media-libs/gstreamer:0 media-sound/lame x11-libs/qt x11-libs/libnotify media-libs/taglib
 """
 get("main/cmake_utils")
