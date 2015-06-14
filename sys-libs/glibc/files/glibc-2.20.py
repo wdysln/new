24c4f6b89288c940d222fab748994afc590a2cd5
wdysln <zirkovandersen@gmail.com>
2015-06-14 21:52:03 +0300

    switch tzdata

--- sys-libs/glibc/files/glibc-2.20.py
+++ sys-libs/glibc/files/glibc-2.20.py
@@ -8,7 +8,7 @@ arch @ ~x86_64
 
 depends = """
 build @ sys-kernel/linux-api-headers
-        sys-libs/timezone-data
+        sys-libs/tzdata
 """
 defaultflags = "-O3 -g -fasynchronous-unwind-tables -mtune=generic -march=x86_64"
 
