b025542df632e5713a841b75b04f880cc1352e42
wdysln <zirkovandersen@gmail.com>
2015-06-14 20:10:56 +0300

    timezone

---
+++ sys-libs/timezone-data/timezone-data-2015d.py
@@ -0,0 +1,27 @@
+metadata = """
+summary @ Sources for time zone and daylight saving time data
+homepage @ http://www.twinsun.com/tz/tz-link.htm
+license @ BSD public-domain
+src_url @ http://www.iana.org/time-zones/repository/releases/$fullname.tar.gz
+arch @ ~x86_64
+"""
+
+standard_procedure = False
+
+timezones=('africa' 'antarctica' 'asia' 'australasia'
+           'europe' 'northamerica' 'southamerica'
+           'pacificnew' 'etcetera' 'backward'
+           'systemv' 'factory')
+
+
+   
+    
+def install():
+    system("zic -y ./yearistype -d %s/usr/share/zoneinfo %s" % (install_dir ,timezones))
+    system("zic -y ./yearistype -d %s/usr/share/zoneinfo/posix %s"  % (install_dir ,timezones))
+    system("zic -y ./yearistype -d %s/usr/share/zoneinfo/right -L leapseconds %s" % (install_dir ,timezones))
+    system("zic -y ./yearistype -d %s/usr/share/zoneinfo/right -L leapseconds %s" % (install_dir ,timezones))
+    
+    system("zic -y ./yearistype -d %s/usr/share/zoneinfo -p America/New_York" % install_dir)
+    #  install -m444 -t ${pkgdir}/usr/share/zoneinfo iso3166.tab zone1970.tab zone.tab 
+   
