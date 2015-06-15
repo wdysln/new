e0233325f4668bc02962434373514ca4fe5125ed
wdysln <zirkovandersen@gmail.com>
2015-06-16 02:32:48 +0300

    xappian

--- dev-libs/xapian-core/xapian-core-1.2.18.py
+++ dev-libs/xapian-core/xapian-core-1.2.18.py
@@ -8,5 +8,14 @@ arch @ ~x86_64
 
 
 
+def configure():
+    export("HOME", build_dir)
+    conf("--disable-static")
+    
+def build():
+    export("HOME", build_dir)
+    make()
 
-
+def install():
+    export("HOME", build_dir)
+    raw_install("DESTDIR=%s" % install_dir)
