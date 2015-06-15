fbffb1b1b0b0b43176522417d367488f306c7c8d
wdysln <zirkovandersen@gmail.com>
2015-06-15 05:07:59 +0300

    up

---
+++ net-misc/networkmanager/networkmanager-1.0.2.py
@@ -0,0 +1,49 @@
+metadata = """
+summary @ Network Management daemon
+homepage @ http://www.gnome.org/projects/NetworkManager/
+license @ GPL
+src_url @ http://ftp.acc.umu.se/pub/gnome/sources/NetworkManager/1.0/NetworkManager-$version.tar.xz
+arch @ ~x86_64
+options @ gnutls dhcpcd introspection
+"""
+
+depends = """
+common @ >=sys-apps/dbus-1.2 dev-libs/dbus-glib net-wireless/wireless_tools sys-apps/systemd
+    sys-auth/polkit dev-libs/libnl:3.0 net-wireless/wpa_supplicant[dbus] net-libs/libsoup
+    >=sys-libs/glib-2.30
+"""
+
+
+srcdir = "NetworkManager-" + version
+
+def prepare():
+    patch(level=1)
+
+def configure():
+    export("HOME", build_dir)
+   # autoreconf("-fi")
+    #system("intltoolize --force --copy --automake")
+    export("NOCONFIGURE", "1")
+    system("./autogen.sh")
+    conf("--prefix=/usr \
+    --sysconfdir=/etc \
+    --localstatedir=/var \
+    --with-crypto=nss \
+    --disable-static \
+    --disable-ppp \
+    --enable-more-warnings=no \
+    --disable-wimax \
+    --enable-doc")
+    
+    
+    
+def install():
+    export("HOME", build_dir)
+    installd()
+    insfile("%s/NetworkManager.conf" % filesdir, "/etc/NetworkManager/")
+    insfile("%s/NetworkManager.confd" % filesdir, "/etc/conf.d/NetworkManager/")
+    
+    insfile("%s/01-org.freedesktop.NetworkManager.settings.modify.system.pkla" % filesdir, "/etc/polkit-1/localauthority/10-vendor.d/")
+
+    makedirs("/etc/NetworkManager/dnsmasq.d")
+
