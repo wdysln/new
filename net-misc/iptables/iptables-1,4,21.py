e9eb0d0ce0f0717d17d7995cfc5843f50ac1ed05
wdysln <zirkovandersen@gmail.com>
2015-06-15 05:20:25 +0300

    up

---
+++ net-misc/iptables/iptables-1,4,21.py
@@ -0,0 +1,14 @@
+metadata = """
+summary @ Linux kernel packet control tool
+homepage @ http://www.netfilter.org/projects/iptables/index.html
+license @ GPL2
+src_url @ http://www.netfilter.org/projects/iptables/files/iptables-1.4.21.tar.bz2
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ sys-libs/glibc dev-libs/openssl
+          sys-fs/sysfsutils
+"""
+
+
