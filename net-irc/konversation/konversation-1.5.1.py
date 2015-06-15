059737289991380dd8955dc4c19a208da7a0cb97
wdysln <zirkovandersen@gmail.com>
2015-06-15 20:33:47 +0300

    konversation

---
+++ net-irc/konversation/konversation-1.5.1.py
@@ -0,0 +1,13 @@
+metadata = """
+summary @ Archiving tool
+homepage @ http://kde.org/
+license @ GPL LGPL FDL 
+src_url @ http://download.kde.org/stable/$version/src/$fullname.tar.xz
+arch @ ~x86_64
+"""
+
+depends = """
+build @ >=kde-base/kde-runtime-4.14.3 >=kde-base/kdelibs-4.14.3 >=kde-base/kde-workspace-4.11.4
+"""
+get("main/kde4_utils")
+
