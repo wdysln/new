2e2911b49b6948c32e5326b33b01a983deb5b907
wdysln <zirkovandersen@gmail.com>
2015-06-16 02:06:21 +0300

    kfilemeta

---
+++ kde-base/kfilemetadata/kfilemetadata-4.14.3.py
@@ -0,0 +1,15 @@
+metadata = """
+summary @  Screen Capture Program
+homepage @  http://kde.org/applications/graphics/ksnapshot/
+license @  GPL + LGPL + FDL
+src_url @  http://download.kde.org/stable/$version/src/$fullname.tar.xz
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ >=kde-base/kde-workspace-4.11.14 >=kde-base/kdelibs-4.14.3
+build @ dev-util/cmake media-libs/taglib/taglib media-gfx/exiv2
+"""
+
+get("main/kde4_utils")
+
