e2606edf71b4b7e78c883fad85b7b0ef475a74d0
wdysln <zirkovandersen@gmail.com>
2015-06-15 05:26:13 +0300

    up

---
+++ net-misc/libndp/libndp-1.5.py
@@ -0,0 +1,13 @@
+metadata = """
+summary @ Library for Neighbor Discovery Protocol
+homepage @ http://libndp.org/
+license @ LGPL2.1
+src_url @ http://libndp.org/files/libndp-1.5.tar.gz
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+
