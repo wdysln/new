1f9b72895778bce77417af9be8a5c9dd27f314d6
wdysln <zirkovandersen@gmail.com>
2015-06-16 09:27:35 +0300

    phonon

--- media-libs/phonon-backend-gstreamer/phonon-backend-gstreamer-4.8.2.py
+++ media-libs/phonon-backend-gstreamer/phonon-backend-gstreamer-4.8.2.py
@@ -4,11 +4,11 @@ homepage @ http://phonon.kde.org/
 license @ LGPL
 src_url @ http://download.kde.org/stable/phonon/phonon-backend-gstreamer/4.8.2/src/phonon-backend-gstreamer-4.8.2.tar.xz
 arch @ ~x86_64
-options @ mplayer vlc
 """
 
 depends = """
-runtime @ x11-libs/qt media-libs/phonon media-libs/gstreamer:1 media-plugins/gst-plugins-base:1
+runtime @ x11-libs/qt media-libs/phonon media-libs/gstreamer:1 media-plugins/gst-plugins-base:1 media-plugins/gst-plugins-good:1
+        media-plugins/gst-plugins-bad:1 media-plugins/gst-plugins-ugly:1
 build @ dev-util/cmake dev-util/automoc4
 """
 
