72bc15f3394d0e36aa3d6606782e6b2363b31f71
wdysln <zirkovandersen@gmail.com>
2015-06-16 05:40:48 +0300

    jannson

--- media-sound/deadbeef/deadbeef-0.6.2.py
+++ media-sound/deadbeef/deadbeef-0.6.2.py
@@ -9,7 +9,7 @@ arch @ ~x86_64
 depends = """
 runtime @ x11-libs/gtk+:2 dev-util/desktop-file-utils
         x11-themes/hicolor-icon-theme media-libs/pango x11-libs/cairo x11-libs/gdk-pixbuf media-libs/alsa-lib
-        media-libs/imlib2 media-libs/libvorbis media-libs/libogg media-video/ffmpeg dev-libs/jansson
+        media-libs/imlib2 media-libs/libvorbis media-libs/libogg media-video/ffmpeg 
         media-plugins/gst-plugins-base:0 media-plugins/gst-plugins-good:0 media-plugins/gst-plugins-bad:0
         media-plugins/gst-plugins-ugly:0 media-libs/gstreamer:0 media-sound/lame x11-libs/libnotify media-libs/taglib        
 """
@@ -18,7 +18,7 @@ srcdir = "deadbeef-master"
 def configure():
     system("/bin/chmod u+x autogen.sh")
     system("./autogen.sh")
-    conf("--prefix=/usr --enable-gtk3=no --enable-staticlink=no")
+    conf("--prefix=/usr --enable-gtk3=no --enable-staticlink=no --without-jansson")
 
 def install():
     raw_install("DESTDIR=%s" % install_dir)
