7e3a81e09e647da3e175e83f5cca3be8dc31d101
wdysln <zirkovandersen@gmail.com>
2015-06-16 04:26:08 +0300

    juk

--- kde-misc/juk/juk-4.14.3.py
+++ kde-misc/juk/juk-4.14.3.py
@@ -8,6 +8,7 @@ arch @ ~x86_64
 
 depends = """
 build @ >=kde-base/kde-runtime-4.14.3 >=kde-base/kdelibs-4.14.3 >=kde-base/kde-workspace-4.11.4
+        media-libs/phonon-backend-gstreamer
 """
 get("main/kde4_utils")
 
---
+++ media-libs/phonon-backend-gstreamer/phonon-backend-gstreamer-4.7.2.py
@@ -0,0 +1,22 @@
+metadata = """
+summary @ GStreamer phonon backend
+homepage @ http://phonon.kde.org/
+license @ LGPL
+src_url @ http://download.kde.org/stable/$name/$version/$fullname.tar.xz
+arch @ ~x86_64
+options @ mplayer vlc
+"""
+
+depends = """
+runtime @ x11-libs/qt media-libs/phonon media-libs/gstreamer/gstreamer:0 media-plugins/gst-plugins-base:0
+build @ dev-util/cmake dev-util/automoc4
+"""
+
+opt_postmerge = """
+mplayer @ media-libs/phonon-mplayer
+vlc @ media-libs/phonon-vlc
+"""
+
+get("main/kde4_utils")
+
+
