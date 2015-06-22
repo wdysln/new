da386f287f0727fc6a4ce8f66bf9d1243de0cd58
wdysln <zirkovandersen@gmail.com>
2015-06-22 06:00:48 +0300

    gdb

--- app-misc/gdb/gdb-7.8.py
+++ app-misc/gdb/gdb-7.8.py
@@ -1,14 +1,14 @@
 metadata = """
-summary @ Desktop note-taking application
-homepage @ http://live.gnome.org/Gnote
+summary @ The GNU Debugger
+homepage @ http://www.gnu.org/software/gdb/
 license @ GPL3
 src_url @ http://ftp.gnu.org/gnu/gdb/gdb-7.8.tar.xz
 arch @ ~x86_64
 """
 
 depends = """
-runtime @ dev-libs/libxslt
-build @ dev-util/intltool app-text/gnome-doc-utils 
+runtime @ dev-libs/libxslt dev-libs/expat sys-libs/ncurses
+build @ sys-apps/texinfo
 """
 
 
