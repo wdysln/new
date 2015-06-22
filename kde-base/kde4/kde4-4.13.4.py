eb20c9dbcaf02c2f5f89249859f2da1ef9e5c99f
wdysln <zirkovandersen@gmail.com>
2015-06-22 05:54:38 +0300

    kde

--- kde-base/kde4/kde4-4.13.4.py
+++ kde-base/kde4/kde4-4.13.4.py
@@ -10,7 +10,8 @@ build @ dev-util/cmake
 runtime @ >=kde-base/kde-runtime-4.14.3 >=kde-base/kdelibs-4.14.3 >=kde-base/kde-workspace-4.11.4
 x11-base/xorg-server >=kde-base/ark-4.14.3 >=kde-base/kde-baseapps-4.14.3 >=kde-base/kate-4.14.3
 >=kde-base/kdepimlibs-4.14.3 >=kde-base/kdepim-runtime-4.14.3 >=kde-base/oxygen-icons-4.14.3
->=kde-base/konsole-4.14.3 kde-base/kmix net-p2p/ktorrent
+>=kde-base/konsole-4.14.3 >=kde-base/kmix-4.14.3 net-p2p/ktorrent >=kde-base/gwenview-4.14.3
+>=kde-base/ksnapshot-4.14.3 >=kde-base/kde-l10n-tr-4.14.3 kde-base/networkmanagement
 """
 
 standard_procedure = False
--- sys-auth/polkit-kde/polkit-kde-0.99.0.py
+++ sys-auth/polkit-kde/polkit-kde-0.99.0.py
@@ -1,13 +1,13 @@
 metadata = """
 summary @ A library that allows developers to access PolicyKit API with a nice Qt-style API
 homepage @ http://www.kde.org/
-license @ LGPL
+license @ GPL
 src_url @ http://download.kde.org/stable/apps/KDE4.x/admin/polkit-kde-agent-1-0.99.0.tar.bz2
 arch @ ~x86_64
 """
 
 depends = """
-runtime @ sys-auth/polkit x11-libs/qt
+runtime @ sys-auth/polkit x11-libs/qt kde-base/kdelibs
 """
 srcdir = "polkit-kde-agent-1-0.99.0"
 
