0ee86b04a06df7749761f07e08526a53b8b8db12
wdysln <zirkovandersen@gmail.com>
2015-06-22 10:54:41 +0300

    libsoup

---
+++ www-client/midori/midori-0.5.10.py
@@ -0,0 +1,23 @@
+metadata = """
+summary @ Lightweight web browser (GTK2)
+homepage @ http://www.midori-browser.org/
+license @ LGPL2.1
+src_url @ "http://www.midori-browser.org/downloads/$name_$version_all_.tar.bz2"
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ app-arch/bzip2 sys-libs/zlib dev-libs/openssl sys-libs/gpm
+build @ media-libs/tiff media-libs/libpng x11-libs/libXt
+"""
+
+srcdir = "links-2.3pre2"
+
+def configure():
+    conf("--enable-javascript",
+            "--disable-graphics",
+            "--without-x",
+            "--without-fb")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
