0a042ee137972c004190b830dcf67a2dcb1c1281
wdysln <zirkovandersen@gmail.com>
2015-07-25 11:09:04 +0300

    unrar

---
+++ app-arch/unrar/unrar-5.2.4.py
@@ -0,0 +1,20 @@
+metadata = """
+summary @ RAR decompressor
+homepage @ http://www.info-zip.org/pub/infozip/Zip.html
+license @ unRAR
+src_url @ http://www.rarlab.com/rar/unrarsrc-5.2.4.tar.gz
+arch @ ~x86_64
+"""
+
+standard_procedure = False
+
+#srcdir = "zip30"
+
+def build():
+    make()
+
+def install():
+    
+    insexe("unrar", "/usr/bin/")
+
+    insdoc("readme.txt","license.txt")
