53ee717a0f11ee2c1ea874e7ac8664d27f3e45a4
wdysln <zirkovandersen@gmail.com>
2015-07-25 11:35:27 +0300

    unrar

--- app-arch/unrar/unrar-5.2.4.py
+++ app-arch/unrar/unrar-5.2.4.py
@@ -1,6 +1,6 @@
 metadata = """
 summary @ RAR decompressor
-homepage @ http://www.info-zip.org/pub/infozip/Zip.html
+homepage @ http://www.rarsoft.com/rar_add.htm
 license @ unRAR
 src_url @ http://www.rarlab.com/rar/unrarsrc-5.2.4.tar.gz
 arch @ ~x86_64
