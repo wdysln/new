5a44875cb2decfffbb4b3b4bbe74f24bfa273fe0
wdysln <zirkovandersen@gmail.com>
2015-10-04 01:24:04 +0300

    upd core

---
+++ dev-libs/icu/icu-55.1.py
@@ -0,0 +1,29 @@
+metadata = """
+summary @ International Components for Unicode library
+homepage @ http://www.icu-project.org
+license @ icu
+src_url @ http://download.icu-project.org/files/icu4c/54.1/icu4c-54_1-src.tgz
+arch @ ~x86_64
+options @ debug examples static-libs
+"""
+
+srcdir = "icu"
+
+depends = """
+runtime @ sys-devel/gcc
+"""
+
+def configure():
+    cd("source")
+    conf(
+        config_enable("debug"),
+        config_enable("examples", "samples"),
+        config_enable("static-libs", "static"))
+
+def build():
+    cd("source")
+    make()
+
+def install():
+    cd("source")
+    raw_install("-j1 DESTDIR=%s" % install_dir)
