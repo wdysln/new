77ae325866513abfe1c3e33054490e6a4b639201
wdysln <zirkovandersen@gmail.com>
2015-10-04 01:51:12 +0300

    upd core

---
+++ sys-apps/kbd/kbd-2.0.3.py
@@ -0,0 +1,20 @@
+metadata = """
+summary @ Keytable files and keyboard utilities
+homepage @ ftp://ftp.altlinux.org/pub/people/legion/kbd/
+license @ GPL
+src_url @ ftp://ftp.altlinux.org/pub/people/legion/$name/$fullname.tar.gz
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+
+def configure():
+    raw_configure("--prefix=/usr",
+            "--datadir=/usr/share/kbd")
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    insdoc("AUTHORS", "ChangeLog", "COPYING", "README")
