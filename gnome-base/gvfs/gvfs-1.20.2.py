a52a9424006373e941d2a9d0169b19f217e0306d
wdysln <zirkovandersen@gmail.com>
2015-10-08 16:31:44 +0000

    gvfs

---
+++ gnome-base/gvfs/gvfs-1.20.2.py
@@ -0,0 +1,34 @@
+metadata = """
+summary @ GNOME Virtual Filesystem Layer
+homepage @ http://www.gnome.org
+license @ LGPL
+src_url @ http://ftp.gnome.org/pub/gnome/sources/$name/1.20/$fullname.tar.xz
+arch @ ~x86_64
+"""
+
+depends = """
+common @ sys-libs/glib sys-apps/dbus dev-libs/libxml2 net-misc/openssh
+"""
+
+def prepare():
+    patch("a.patch", level=1)
+
+def configure():
+    conf("--with-dbus-service-dir=/usr/share/dbus-1/services \
+         --disable-hal \
+         --disable-silent-rules \
+         --disable-static \
+         --enable-afc \
+         --enable-archive \
+         --enable-bash-completion \
+         --enable-bluray \
+         --enable-gphoto2 \
+         --enable-keyring \
+         --enable-udev \
+         --enable-udisks2")
+    
+    
+
+
+
+
