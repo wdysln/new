9bc492b6997f9ed4f4106f7958297996ac9da46e
wdysln <zirkovandersen@gmail.com>
2015-10-09 16:46:59 +0000

    fltk

---
+++ x11-libs/fltk/fltk-1.3.3.py
@@ -0,0 +1,23 @@
+metadata = """
+summary @ Graphical user interface toolkit for X
+homepage @ http://www.fltk.org/
+license @ custom LGPL
+src_url @ http://fltk.org/pub/fltk/$version/fltk-$version-source.tar.gz
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ dev-libs/expat
+build @ dev-util/pkg-config
+"""
+
+
+def configure():
+    conf("--enable-shared") 
+            
+
+def build():
+    make()
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
