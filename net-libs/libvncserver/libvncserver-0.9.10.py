845dbbdf6f619ff8e8f721f6bc6b3afdea695853
wdysln <zirkovandersen@gmail.com>
2015-10-10 12:57:28 +0300

    libvnc

--- net-libs/libvncserver/libvncserver-0.9.10.py
+++ net-libs/libvncserver/libvncserver-0.9.10.py
@@ -11,6 +11,8 @@ runtime @ net-libs/gnutls dev-libs/libgcrypt dev-libs/openssl media-libs/SDL
           media-libs/libjpeg-turbo media-libs/libpng
 build @ dev-util/cmake dev-libs/boost dev-util/automoc4
 """
+srcdir = "%s-LibVNCServer-%s" % (name, version)
+
 
 def configure():
     system("./autogen.sh --prefix=/usr")
