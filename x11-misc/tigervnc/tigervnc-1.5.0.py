7fca69b58640dbc7c0d2891618f8d3118ae785aa
wdysln <zirkovandersen@gmail.com>
2015-10-10 20:11:17 +0300

    tigervnc

--- x11-misc/tigervnc/tigervnc-1.5.0.py
+++ x11-misc/tigervnc/tigervnc-1.5.0.py
@@ -9,13 +9,8 @@ arch @ ~x86_64
 
 
 depends = """
-runtime @ sys-libs/zlib media-libs/freetype x11-libs/libSM x11-libs/libXtst
+runtime @ sys-libs/zlib media-libs/freetype x11-libs/libSM 
           x11-base/xorg-server x11-proto/bigreqsproto x11-proto/compositeproto
-          x11-proto/damageproto x11-proto/dri2proto x11-proto/fixesproto x11-proto/fontsproto
-          x11-proto/randrproto x11-proto/resourceproto x11-proto/scrnsaverproto x11-proto/trapproto
-          x11-proto/videoproto x11-proto/xcmiscproto x11-proto/xineramaproto
-          x11-proto/xf86bigfontproto x11-proto/xf86dgaproto x11-proto/xf86driproto
-          x11-proto/xf86miscproto x11-proto/xf86vidmodeproto x11-proto/glproto 
           media-libs/mesa x11-proto/renderproto x11-libs/libpciaccess x11-libs/xtrans
           
 build @ dev-lang/yasm x11-proto/inputproto x11-proto/xextproto x11-proto/xproto x11-libs/fltk
