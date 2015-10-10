c3fa46ec0543fc0c425420329642e860f6104411
wdysln <zirkovandersen@gmail.com>
2015-10-10 17:24:42 +0300

    json-c

---
+++ dev-libs/json-c/json-c-0.12.py
@@ -0,0 +1,19 @@
+metadata = """
+summary @ A JSON implementation in C
+homepage @ https://github.com/json-c/json-c/wiki
+license @ MIT
+src_url @ https://s3.amazonaws.com/json-c_releases/releases/json-c-0.12.tar.gz
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ sys-libs/glibc
+"""
+
+def configure():
+    conf("--disable-static")
+
+def build():
+    make()
+    make("check")
+
