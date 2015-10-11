2cafbb00926a703f2ef1c2372e6a38378c2a6fc1
wdysln <zirkovandersen@gmail.com>
2015-10-11 21:39:46 +0000

    perl htm

--- dev-perl/HTML-Parser/HTML-Parser-3.68.py
+++ dev-perl/HTML-Parser/HTML-Parser-3.68.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ Perl HTML parser class
 homepage @ http://search.cpan.org/dist/HTML-Parser
 license @ LGPL
-src_url @ http://www.cpan.org/authors/id/G/GA/GAAS/$fullname.tar.gz
+src_url @ http://pkgs.fedoraproject.org/repo/pkgs/perl-HTML-Parser/HTML-Parser-3.68.tar.gz/5550b2da7aa94341f1e8a17a4ac20c68/HTML-Parser-3.68.tar.gz
 arch @ ~x86_64
 """
 
