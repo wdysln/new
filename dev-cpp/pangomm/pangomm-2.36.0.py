3afe5b004b0a0b77b8348a84809b6dc073e382bb
wdysln <zirkovandersen@gmail.com>
2015-10-15 23:16:32 +0300

    atkmm

--- dev-cpp/pangomm/pangomm-2.36.0.py
+++ dev-cpp/pangomm/pangomm-2.36.0.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ C++ bindings for pango
 homepage @ http://gtkmm.sourceforge.net/
 license @ LGPL
-src_url @ http://ftp.gnome.org/pub/GNOME/sources/$name/2.38/$fullname.tar.bz2
+src_url @ http://ftp.gnome.org/pub/GNOME/sources/$name/2.36/$fullname.tar.xz
 arch @ ~x86_64
 """
 
