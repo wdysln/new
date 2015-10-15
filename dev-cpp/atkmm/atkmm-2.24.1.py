01393217bd2a94b44fb23d6e5fcecff97ec68918
wdysln <zirkovandersen@gmail.com>
2015-10-15 23:09:34 +0300

    atkmm

--- dev-cpp/atkmm/atkmm-2.24.1.py
+++ dev-cpp/atkmm/atkmm-2.24.1.py
@@ -2,7 +2,7 @@ metadata = """
 summary @ C++ bindings for atk
 homepage @ http://gtkmm.sourceforge.net
 license @ LGPL
-src_url @ http://ftp.gnome.org/pub/GNOME/sources/$name/2.22/$fullname.tar.bz2
+src_url @ http://ftp.gnome.org/pub/GNOME/sources/$name/2.24/$fullname.tar.bz2
 arch @ ~x86_64
 """
 
