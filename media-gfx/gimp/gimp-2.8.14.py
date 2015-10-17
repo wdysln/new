f1ffd47078641f6bf69e8f5df69017da0964eff1
wdysln <zirkovandersen@gmail.com>
2015-10-17 19:46:32 +0300

    babl

--- media-gfx/gimp/gimp-2.8.14.py
+++ media-gfx/gimp/gimp-2.8.14.py
@@ -7,7 +7,35 @@ arch @ ~x86_64
 options @ alsa aalib altivec curl dbus debug doc exif gnome jpeg lcms mmx mng pdf png python smp sse svg tiff webkit wmf udev
 slot @ 2
 """
+depends = """
+runtime @ >=sys-libs/glib-2.18.1 dev-libs/libxml2 dev-libs/libxslt 
+>=media-libs/fontconfig-2.2.0 >=media-libs/freetype-2.1.7
+>=media-libs/gegl-0.1.8 >=x11-libs/gtk+-2.24.7:2
+x11-libs/libXpm >=media-libs/pango-1.18.0 sys-libs/zlib x11-misc/xdg-utils
+x11-themes/hicolor-icon-theme x11-libs/libXcursor
+build @ >=dev-util/intltool-0.40 >=dev-util/pkg-config-0.12.0
+>=sys-devel/gettext-0.17 >=media-libs/babl-0.1.12 dev-util/gtk-doc
+"""
 
+opt_runtime = """
+aalib @ media-libs/aalib
+alsa @ media-libs/alsa-lib 
+curl @ net-misc/curl 
+dbus @ dev-libs/dbus-glib 
+exif @ >=media-libs/libexif-0.6.15 
+gnome @ gnome-base/gvfs 
+jpeg @ media-libs/jpeg
+lcms @ media-libs/lcms
+mng @ media-libs/libmng 
+pdf @ >=app-text/poppler-0.12.3-r3[cairo]
+png @ >=media-libs/libpng-1.2.2
+python @ >=dev-python/pygtk-2.10.4
+svg @ >=gnome-base/librsvg-2.8.0
+tiff @ >=media-libs/tiff-3.5.7 
+webkit @ net-libs/webkit-gtk
+wmf @ >=media-libs/libwmf-0.2.8
+udev @ sys-fs/udev[gudev]
+"""
 
 get("fdo_mime")
 get("gnome2_utils")
@@ -15,12 +43,9 @@ get("gnome2_utils")
 """
 def prepare():
     patch("gimp-2.7.4-no-deprecation.patch")
-    patch("gimp-2.7.5-configure-gs-bzip2.patch", level=1)
 """
 
 def configure():
-  #  libtoolize("-f")
-   # autoreconf()
     conf(
     "--disable-dependency-tracking",
     "--enable-default-binary",
@@ -46,8 +71,6 @@ def configure():
     config_enable("sse"),
     config_with("svg", "librsvg"),
     config_with("tiff", "libtiff"),
-    config_with("wmf"),
-    config_with("udev", "gudev"))
 
 def build():
     export("PYTHONDONTWRITEBYTECODE", "1")
