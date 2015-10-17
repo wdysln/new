15fa447f4eeec0ae5285336639d180a7e8354f8c
wdysln <zirkovandersen@gmail.com>
2015-10-17 13:03:14 +0300

    docker

---
+++ dev-lang/docker/docker-1.7.1.py
@@ -0,0 +1,44 @@
+metadata = """
+summary @  Compiler and tools for the Go programming language from Google
+homepage @ http://golang.org/
+license @ BSD
+src_url @ https://github.com/docker/docker/archive/v1.7.1.tar.gz
+arch @ ~x86_64
+options @ debug threads
+"""
+
+#srcdir = "%s-%s%s" % (name ,name, version)
+
+standard_procedure = False
+
+def prepare():
+    export("AUTO_GOPATH", "1")
+    export("DOCKER_GITCOMMIT", "786b29d") 
+    export("GOPATH", "%s" % srcdir)
+    export("CGO_CFLAGS", "-I/usr/include")
+    export("CGO_LDFLAGS", "-L/usr/lib")
+    export("DOCKER_BUILDTAGS","exclude_graphdriver_aufs")
+    export("DOCKER_INITPATH", "/usr/lib/docker/dockerinit")
+
+def build():
+    system("./hack/make.sh dynbinary")
+
+
+def install():  
+    
+    insexe("bundles/1.7.1/dynbinary/docker","/usr/bin/")
+    insexe("bundles/1.7.1/dynbinary/docker-1.7.1","/usr/bin/")
+    insexe("bundles/1.7.1/dynbinary/dockerinit", "/usr/lib/dockerinit")
+    insexe("bundles/1.7.1/dynbinary/dockerinit-1.7.1", "/usr/lib/dockerinit-1.7.1")
+
+    insinto("contrib/init/systemd/docker.service", "/usr/lib/systemd/system")
+    insinto("contrib/init/systemd/docker.socket", "/usr/lib/systemd/system")
+    insinto("%s/docker.conf" % filesdir ,"/usr/lib/sysusers.d")
+
+def post_install():
+    system("systemd-sysusers docker.conf")
+
+
+    #dodoc("VERSION", "LICENSE", "README.md", "AUTHORS", "CONTRIBUTING.md", "CHANGELOG.md", "NOTICE")
+    
+
