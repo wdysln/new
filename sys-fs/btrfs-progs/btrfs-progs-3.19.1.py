af07ce06880275dff45d2ee184eff4204598c2bf
wdysln <zirkovandersen@gmail.com>
2015-10-17 11:14:41 +0300

    btrfs

---
+++ sys-fs/btrfs-progs/btrfs-progs-3.19.1.py
@@ -0,0 +1,26 @@
+metadata = """
+summary @ Btrfs filesystem utilities
+homepage @ http://btrfs.wiki.kernel.org
+license @ GPL2
+src_url @ http://source.pisilinux.org/1.0/btrfs-progs-v3.19.1.tar.xz
+arch @ ~x86_64
+"""
+
+depends = """
+common @ sys-apps/util-linux app-shells/bash
+"""
+
+def prepare():
+    sed("-i '/init\.d/s|^|#|' misc/Makefile.in")
+
+def configure():
+    system("./autogen.sh")
+    conf()
+
+def install():
+    installd()
+
+    insexe("bcp", "btrfs-bcp","/usr/bin")
+    insexe("show-blocks", "btrfs-show-blocks","/usr/bin")
+
+
