c6719e02272e894efbf6bf9dd9e9c12794c6652a
wdysln <zirkovandersen@gmail.com>
2015-10-22 22:27:36 +0300

    xfce

--- xfce-base/xfce4/xfce4-4.10.py
+++ xfce-base/xfce4/xfce4-4.10.py
@@ -9,7 +9,9 @@ depends = """
 build @ dev-perl/XML-Parser x11-misc/xcb-util x11-base/xorg-server
 runtime @ >=xfce-base/xfwm4-4.9 >=xfce-extra/xfce4-appfinder-4.9 >=xfce-base/xfdesktop-4.9 >=xfce-base/xfce4-session-4.9  
 		  xfce-base/thunar xfce-base/xfce4-settings 
-postmerge @ x11-terms/xfce4-terminal xfce-extra/xfce4-mixer dev-util/geany
+postmerge @ x11-terms/xfce4-terminal xfce-extra/xfce4-mixer dev-util/geany sys-fs/ntfs_3g 
+			sys-fs/udisks sys-power/upower sys-auth/polkit-gnome gnome-base/gvfs lxde-base/lxdm
+			x11-themes/faenza-icon-theme x11-themes/xfce-theme-greybird	lxde-base/lxdm	
 """
 
 standard_procedure = False
