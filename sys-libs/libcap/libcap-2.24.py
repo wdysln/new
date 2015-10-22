1236af25cd9575a2bf48fa26509a34dfc78dedff
wdysln <zirkovandersen@gmail.com>
2015-10-22 06:17:44 +0300

    libcap

--- sys-libs/libcap/libcap-2.24.py
+++ sys-libs/libcap/libcap-2.24.py
@@ -12,6 +12,6 @@ runtime @ sys-libs/glibc sys-apps/attr
 def build():
     make("lib_prefix=/usr")	
 def install():
-    raw_install("prefix=/usr DESTDIR=%s RAISE_SETFCAP=no" % install_dir)
-    setmod("755 %s/usr/lib64/libcap.so.%s" % (install_dir, raw_version))
-    rmfile("/usr/lib64/libcap.a")
+    raw_install("prefix=/usr lib=/lib DESTDIR=%s RAISE_SETFCAP=no" % install_dir)
+  #  setmod("755 %s/usr/lib64/libcap.so.%s" % (install_dir, raw_version))
+   # rmfile("/usr/lib64/libcap.a")
