a746aae73e65811af3d641a2544003139c4292d6
wdysln <zirkovandersen@gmail.com>
2015-10-23 01:28:03 +0300

    Update xfce4-4.10.py

--- xfce-base/xfce4/xfce4-4.10.py
+++ xfce-base/xfce4/xfce4-4.10.py
@@ -6,7 +6,7 @@ arch @ ~x86_64
 """
 
 depends = """
-build @ dev-perl/XML-Parser x11-misc/xcb-util x11-base/xorg-server
+build @ dev-perl/XML-Parser x11-base/xorg-server
 runtime @ >=xfce-base/xfwm4-4.9 >=xfce-extra/xfce4-appfinder-4.9 >=xfce-base/xfdesktop-4.9 >=xfce-base/xfce4-session-4.9  
 		  xfce-base/thunar xfce-base/xfce4-settings 
 postmerge @ x11-terms/xfce4-terminal xfce-extra/xfce4-mixer dev-util/geany sys-fs/ntfs_3g 
