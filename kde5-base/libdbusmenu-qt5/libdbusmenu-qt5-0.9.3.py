d3f812cf01eabc2a73b35d926378803070d5ee75
wdysln <zirkovandersen@gmail.com>
2015-10-23 09:07:26 +0300

    libdbusmenu

---
+++ kde5-base/libdbusmenu-qt5/libdbusmenu-qt5-0.9.3.py
@@ -0,0 +1,19 @@
+metadata = """
+summary @ A library that provides a Qt implementation of the DBusMenu spec
+homepage @ https://launchpad.net/libdbusmenu-qt
+license @ GPL
+src_url @ http://www.linuxfromscratch.org/~krejzi/libdbusmenu-qt-0.9.3+15.10.20150604.tar.xz
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ x11-libs/qt5
+build @ dev-util/cmake net-misc/wget[ssl,gnutls] dev-libs/qjson
+"""
+src
+get("main/cmake_utils")
+
+def configure():
+    cmake_conf("-DCMAKE_INSTALL_PREFIX=/usr \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_INSTALL_LIBDIR=lib")
