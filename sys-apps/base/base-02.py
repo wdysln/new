47467ea17be9ba470f2ac45575e50967edec15b2
wdysln <zirkovandersen@gmail.com>
2015-10-30 16:33:56 +0000

    core

--- sys-apps/base/base-02.py
+++ sys-apps/base/base-02.py
@@ -8,7 +8,7 @@ slot @ 2
 
 depends = """
 runtime @ sys-apps/lpms sys-devel/make sys-devel/automake sys-devel/autoconf app-editors/nano app-arch/tar app-arch/bzip2 app-arch/gzip sys-apps/grep sys-apps/gawk sys-apps/diffutils sys-apps/baselayout net-misc/wget app-arch/xz sys-process/procps sys-apps/less dev-vcs/git sys-devel/libtool sys-apps/kbd
-net-misc/dhcpcd sys-libs/zlib x11-proto/xproto x11-misc/util-macros sys-apps/util-linux sys-apps/texinfo dev-util/pkg-config sys-devel/patch sys-process/lsof sys-apps/dbus sys-apps/systemd sys-devel/bison app-admin/syslog-ng sys-fs/sysfsutils sys-apps/net-tools dev-libs/eventlog net-misc/iputils sys-apps/iproute2 
+net-misc/dhcpcd sys-libs/zlib sys-apps/util-linux sys-apps/texinfo dev-util/pkg-config sys-devel/patch sys-process/lsof sys-apps/dbus sys-apps/systemd sys-devel/bison app-admin/syslog-ng sys-fs/sysfsutils sys-apps/net-tools dev-libs/eventlog net-misc/iputils sys-apps/iproute2 
 sys-apps/inetutils
 """
 
