22d49808bd85efd0f5804171b70dc3cfaafc9fe0
wdysln <zirkovandersen@gmail.com>
2015-10-30 19:58:41 +0000

    pyth

--- dev-lang/python/python-2.7.10.py
+++ dev-lang/python/python-2.7.10.py
@@ -3,82 +3,45 @@ summary @ A high-level scripting language
 homepage @ http://www.python.org
 license @ GPL-2
 src_url @ http://www.python.org/ftp/python/$version/Python-$version.tar.xz
-options @ gdbm berkdb ncurses readline ssl xml ipv6 threads wide-unicode examples
+options @ ipv6 examples
 arch @ ~x86_64
 slot @ 2.7
 """
 srcdir = "Python-2.7.10"
 
-# TODO:
-# tk support
 
 depends = """
 common @ dev-db/sqlite app-arch/bzip2 >=sys-libs/zlib-1.1.3 
-dev-libs/libffi dev-util/intltool
+dev-libs/libffi dev-util/intltool sys-libs/gdbm
 """
 
-opt_common = """
-berkdb @ sys-libs/db
-gdbm @ sys-libs/gdbm
-ncurses @ >=sys-libs/ncurses-5.2
-    readline @ >=sys-libs/readline-4.1
-ssl @ dev-libs/openssl
-xml @ dev-libs/expat
-"""
 
 def prepare():
     sed('-i "/progname =/s/python/python2.7/" Python/pythonrun.c')
-    patch(location="%s/2.7.10" % filesdir)
+    patch(level=1)
     for i in ('expat', 'zlib', '_ctypes/darwin'): #'_ctypes/libffi'):
         rmdir('Modules/%s' % i)
     copytree("Lib/plat-linux2", "%s/Lib/plat-linux3" % build_dir)
     rmfile("Lib/distutils/command/*.exe")
+    append_cflags("-fwrapv")
 
 def configure():
-    disable = []; dbmliborder=[]
-    if not opt("berkdb") and not opt("gdbm"):
-        disable.append("dbm")
-    
-    if not opt("gdbm"):
-        disable.append("gdbm")
-    else:
-        dbmliborder.append("gdbm")
-    if not opt("berkdb"):
-        disable.append("_bsddb")
-    else:
-        dbmliborder.append("bdb")
-
-    if not opt("ncurses"):
-        disable.append("_curses _curses_panel")
-    if not opt("readline"):
-        disable.append("readline")
-    if not opt("ssl"):
-        export("PYTHON_DISABLE_SSL", "1")
-    if not opt("xml"):
-        warn("you have configured Python without XML support")
-        warn("this is not recommended configuration as you")
-        disable.append("_elementtree pyexpat")
-    
-    if disable:
-        export("PYTHON_DISABLE_MODULES", " ".join(disable))
-
     raw_configure("--prefix=/usr",
             "--with-fpectl",
             "--infodir=/usr/share/info",
             "--mandir=/usr/share/man",
             "--with-libc=''",
             config_enable("ipv6"),
-            config_with("threads"),
-            "--enable-unicode=ucs4" if opt("wide-unicode") \
-                    else "--enable-unicode=ucs2",
-            "--with-dbmliborder=%s" % ":".join(dbmliborder),
+            "--with-threads",
+            "--enable-unicode=ucs4",
+            "--with-dbmliborder=gdbm",
             "--enable-shared",
             "--with-system-expat",
             "--with-system-ffi")
 
-#def build():
+def build():
     #export("PYTHONDONTWRITEBYTECODE", "1")
-    #make()
+    make()
 
 def install():
     raw_install("DESTDIR=%s altinstall maninstall" % install_dir)
@@ -90,13 +53,6 @@ def install():
     for item in ('idlelib', 'lib-tk'):
         rmdir(joinpath(libdir, item))
 
-    if not opt("berkdb"):
-        rmfile(joinpath(libdir, "dbhash.py"))
-        rmfile(joinpath(libdir, "test/test_bsddb*"))
-        rmdir(joinpath(libdir, "bsddb"))
-
-    if not opt("threads"):
-        rmdir(joinpath(libdir, "multiprocessing"))
 
     if opt("examples"):
         makedirs("/usr/share/doc/%s" % fullname)
