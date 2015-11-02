915a8f04088dd1b65095357ac690aecbddcff079
wdysln <zirkovandersen@gmail.com>
2015-11-02 13:11:03 +0000

    base

--- sys-apps/base/base-02.py
+++ sys-apps/base/base-02.py
@@ -12,7 +12,8 @@ app-arch/bzip2 app-arch/gzip sys-apps/grep sys-apps/gawk sys-apps/diffutils sys-
 net-misc/wget app-arch/xz sys-process/procps sys-apps/less dev-vcs/git sys-devel/libtool sys-apps/kbd
 net-misc/dhcpcd sys-libs/zlib sys-apps/util-linux sys-apps/texinfo dev-util/pkg-config sys-devel/patch 
 sys-process/lsof sys-apps/dbus sys-apps/systemd sys-devel/bison app-admin/syslog-ng sys-fs/sysfsutils sys-apps/net-tools
-net-misc/iputils sys-apps/iproute2 sys-apps/inetutils
+net-misc/iputils sys-apps/iproute2 sys-apps/inetutils dev-libs/isl dev-libs/libsigsegv dev-libs/libunistring
+dev-libs/guile dev-libs/gc
 """
 
 standard_procedure = False
