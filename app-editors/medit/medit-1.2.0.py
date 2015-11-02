ed472f9b14bb09fcc40fd47cfdc6621387a217e9
wdysln <zirkovandersen@gmail.com>
2015-11-02 13:42:57 +0000

    medit

---
+++ app-editors/medit/medit-1.2.0.py
@@ -0,0 +1,24 @@
+metadata = """
+summary @ A GTK text editor
+homepage @ http://mooedit.sourceforge.net
+license @ GPL
+src_url @ http://downloads.sourceforge.net/mooedit/$name-$version.tar.bz2
+arch @ ~x86_64
+"""
+
+depends = """
+runtime @ x11-libs/gtk+:2 sys-libs/glib x11-libs/vte
+build @ dev-util/pkg-config dev-util/intltool dev-python/pygtk x11-libs/libSM
+"""
+
+get("gnome2_utils")
+
+def configure():
+    conf()
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+
+def post_install():
+    gnome2_icon_cache_update()
+
