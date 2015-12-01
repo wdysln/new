16d9755525a8bab1a43e80cdbb00d1be2dbc50fd
wdysln <zirkovandersen@gmail.com>
2015-12-02 00:26:32 +0200

    pcmanfm

--- x11-misc/pcmanfm/pcmanfm-1.2.1.py
+++ x11-misc/pcmanfm/pcmanfm-1.2.1.py
@@ -2,12 +2,12 @@ metadata = """
 summary @ Fast lightweight tabbed filemanager
 homepage @ http://pcmanfm.sourceforge.net/
 license @ GPL-2
-src_url @ http://downloads.sourceforge.net/$name/$fullname.tar.gz
+src_url @ http://downloads.sourceforge.net/$name/$fullname.tar.xz
 arch @ ~x86_64
 """
 
 depends = """
-common @ sys-libs/glib x11-libs/gtk+:2 >=lxde-base/menu-cache
+common @ sys-libs/glib x11-libs/gtk+:2 lxde-base/menu-cache
 lxde-base/libfm
 """
 
