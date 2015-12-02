84c5614803d0e43006d296874dc80abc9eb9e3cc
wdysln <zirkovandersen@gmail.com>
2015-12-02 02:53:30 +0200

    nss

--- dev-libs/nss/nss-3.21.py
+++ dev-libs/nss/nss-3.21.py
@@ -22,21 +22,21 @@ def build():
 def install():
     for binary in ("certutil", "cmsutil", "crlutil", "modutil", "pk12util", \
             "shlibsign", "signtool", "signver", "ssltap", "nss-config"):
-        insinto("Linux*/bin/%s" % binary, "/usr/bin", sym=False)
+        insinto("dist/Linux*/bin/%s" % binary, "/usr/bin", sym=False)
 
     for lib in ["*.chk","*.so","libcrmf.a"]:
-        insinto("Linux*/lib/%s" % lib, "/usr/lib", sym=False)
+        insinto("dist/Linux*/lib/%s" % lib, "/usr/lib", sym=False)
 
    # insinto("mozilla/dist/*.OBJ/lib/libcrmf.a", "/usr/lib/", sym=False)
 
     # Headers
-    for header in ["private/nss/*.h", "public/nss/*.h"]:
+    for header in ["dist/private/nss/*.h", "dist/public/nss/*.h"]:
         insinto(header, "/usr/include/nss", sym=False)
 
     # Drop executable bits from headers
     setmod("0644", "%s/usr/include/nss/*.h" % install_dir)
 
-    insfile("Linux*/lib/pkgconfig/nss.pc", "/usr/lib/pkgconfig/nss.pc")
+    insfile("dist/Linux*/lib/pkgconfig/nss.pc", "/usr/lib/pkgconfig/nss.pc")
 
   #  for lib in ('libssl3.so', 'libsmime3.so', 'libnssutil3.so', 'libnss3.so', 
    #         'libsoftokn3.so', 'libfreebl3.so', 'libnssckbi.so', 'libnssdbm3.so'):
