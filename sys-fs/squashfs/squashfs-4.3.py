3d88fd9b93cd2c46c894c3342c295d148c317766
wdysln <zirkovandersen@gmail.com>
2015-12-02 05:57:51 +0200

    squa

--- sys-fs/squashfs/squashfs-4.3.py
+++ sys-fs/squashfs/squashfs-4.3.py
@@ -2,13 +2,13 @@ metadata = """
 summary @ Tools for squashfs, a highly compressed read-only filesystem for Linux.
 homepage @ http://squashfs.sourceforge.net
 license @ GPL
-src_url @ http://downloads.sourceforge.net/sourceforge/$name/$fullname.tar.gz
+src_url @ http://downloads.sourceforge.net/sourceforge/$name/squashfs4.3.tar.gz
 arch @ ~x86_64
 """
 depends = """
 common @ app-arch/lzo app-arch/xz sys-libs/zlib
 """
-
+#srcdir =""        
 def build():
     make("XZ_SUPPORT=1 LZO_SUPPORT=1 LZMA_XZ_SUPPORT=1")
 
